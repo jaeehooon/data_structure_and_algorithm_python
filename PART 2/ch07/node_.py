d30d11a6adf3e86cd2ced03a403818232e3101c5
jaehooon <qlenfr0922@gmail.com>
2021-01-09 23:13:38 +0900

    210109 23:13

---
+++ PART 2/ch07/node_.py
@@ -0,0 +1,29 @@
+# 7.5 연결 리스트
+class Node(object):
+    def __init__(self, value=None, pointer=None):
+        self.value = value
+        self.pointer = pointer
+
+    def getData(self):
+        return self.value
+
+    def getNext(self):
+        return self.pointer
+
+    def setData(self, newData):
+        self.value = newData
+
+    def setNext(self, newPointer):
+        self.pointer = newPointer
+
+
+if __name__ == '__main__':
+    L = Node("a", Node("b", Node("c", Node("d"))))
+    assert(L.pointer.pointer.value == "c")
+
+    print(L.getData())
+    print(L.getNext().getData())
+    L.setData("aa")
+    L.setNext(Node("e"))
+    print(L.getData())
+    print(L.getNext().getData())
