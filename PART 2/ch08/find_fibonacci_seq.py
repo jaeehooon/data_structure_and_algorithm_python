9c9ff33d064893eb2e5ab398af0a1426727a5dbb
jaehooon <qlenfr0922@gmail.com>
2021-01-16 15:45:55 +0900

    210116 15:45

---
+++ PART 2/ch08/find_fibonacci_seq.py
@@ -0,0 +1,9 @@
+# 8.3 함수의 실행 시간
+def find_fibonacci_seq_rec(n):
+    if n < 2:
+        return n
+    return find_fibonacci_seq_rec(n - 1) + find_fibonacci_seq_rec(n - 2)
+
+
+if __name__ == '__main__':
+    print(find_fibonacci_seq_rec(5))
